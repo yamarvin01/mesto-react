6ada7d1a8b07d3e85ad39d21fe63ebb64cfd1ab3
Marsel Ishmukhametov <yamarvin01@yandex.ru>
2022-09-21 22:58:48 +0300

    PW10 - Исправление замечаний ревью

--- src/components/ImagePopup/ImagePopup.js
+++ src/components/ImagePopup/ImagePopup.js
@@ -9,16 +9,15 @@ export default class ImagePopup extends React.Component {
   // className={`popup popup_dark popup_type_${this.props.name} ${this.props.isOpen}`}>
   render() {
     return (
-      <div className={this.props.isOpen ? 'popup popup_opened' : 'popup'}>
+      <div className={this.props.isOpen ? "popup popup_opened" : "popup"}>
         <div className="popup__container popup__container_type_image">
+          <ButtonClose onClose={this.props.onClose} />
           <img
             className="popup__image"
             alt={`Изображение ${this.props.card.name}`}
             src={this.props.card.link}
           />
           <p className="popup__text">{this.props.card.name}</p>
-
-          <ButtonClose onClose={this.props.onClose} />
         </div>
       </div>
     );
--- src/components/PopupWithForm/PopupWithForm.js
+++ src/components/PopupWithForm/PopupWithForm.js
@@ -13,18 +13,14 @@ export default class PopupWithForm extends React.Component {
       <div className={this.props.isOpen ? "popup popup_opened" : "popup"}>
         <div className={`popup__container popup__container_type_${this.props.name}`}>
           <h2 className="popup__title">{this.props.title}</h2>
-
           <ButtonClose onClose={this.props.onClose} />
-          
           <form
             className={`popup__form popup__form_type_${this.props.name}`}
             name={this.props.name}
             noValidate
           >
             {this.props.children}
-
             <ButtonSubmit>{this.props.btnText}</ButtonSubmit>
-
           </form>
         </div>
       </div>
