7a6a41350142fb395ad28325f4b83e9d97075155
Marsel Ishmukhametov <yamarvin01@yandex.ru>
2022-09-21 19:46:35 +0300

    PW10 - Исправление замечаний ревью

--- src/components/App.js
+++ src/components/App.js
@@ -21,6 +21,80 @@ export default class App extends React.Component {
       isImagePopupOpen: false,
       selectedCard: {},
     };
+
+    this.editAvatarFormChildren = (
+      <label className="popup__field" htmlFor="avatar-input">
+        <input
+          id="avatar-input"
+          className="popup__input popup__input_type_avatar"
+          type="url"
+          name="avatar"
+          placeholder="Ссылка на аватар"
+          required
+        />
+        <span className="avatar-input-error popup__error"></span>
+      </label>
+    );
+
+    this.editProfileFormChildren = (
+      <>
+        <label className="popup__field" htmlFor="name-input">
+          <input
+            id="name-input"
+            className="popup__input popup__input_type_name"
+            type="text"
+            name="name"
+            placeholder="Имя"
+            minLength="2"
+            maxLength="40"
+            required
+          />
+          <span className="name-input-error popup__error"></span>
+        </label>
+        <label className="popup__field" htmlFor="about-input">
+          <input
+            id="about-input"
+            className="popup__input popup__input_type_text"
+            type="text"
+            name="about"
+            placeholder="О себе"
+            minLength="2"
+            maxLength="200"
+            required
+          />
+          <span className="about-input-error popup__error"></span>
+        </label>
+      </>
+    );
+
+    this.addPlaceFormChildren = (
+      <>
+        <label className="popup__field" htmlFor="place-input">
+          <input
+            id="place-input"
+            className="popup__input popup__input_type_place"
+            type="text"
+            name="place"
+            placeholder="Название"
+            minLength="2"
+            maxLength="30"
+            required
+          />
+          <span className="place-input-error popup__error"></span>
+        </label>
+        <label className="popup__field" htmlFor="link-input">
+          <input
+            id="link-input"
+            className="popup__input popup__input_type_link"
+            type="url"
+            name="link"
+            placeholder="Ссылка на картинку"
+            required
+          />
+          <span className="link-input-error popup__error"></span>
+        </label>
+      </>
+    );
   }
 
   componentDidMount() {
@@ -64,7 +138,7 @@ export default class App extends React.Component {
       isAddPlacePopupOpen: false,
       isDeleteCardPopupOpen: false,
       isImagePopupOpen: false,
-      selectedCard: {}
+      selectedCard: {},
     });
   };
 
@@ -87,6 +161,7 @@ export default class App extends React.Component {
           name="editAvatar"
           title="Обновить аватар"
           btnText="Сохранить"
+          children={this.editAvatarFormChildren}
           isOpen={this.state.isEditAvatarPopupOpen}
           onClose={this.closeAllPopups}
         />
@@ -94,6 +169,7 @@ export default class App extends React.Component {
           name="editProfile"
           title="Редактировать профиль"
           btnText="Сохранить"
+          children={this.editProfileFormChildren}
           isOpen={this.state.isEditProfilePopupOpen}
           onClose={this.closeAllPopups}
         />
@@ -101,6 +177,7 @@ export default class App extends React.Component {
           name="addPlace"
           title="Новое место"
           btnText="Создать"
+          children={this.addPlaceFormChildren}
           isOpen={this.state.isAddPlacePopupOpen}
           onClose={this.closeAllPopups}
         />
--- src/components/PopupWithForm/PopupWithForm.js
+++ src/components/PopupWithForm/PopupWithForm.js
@@ -8,7 +8,7 @@ export default class PopupWithForm extends React.Component {
   // <div className={`popup popup_type_${this.props.name} ${this.props.isOpen} ? popup_opened : ''`}>
   render() {
     return (
-      <div className={this.props.isOpen ? 'popup popup_opened' : 'popup'}>
+      <div className={this.props.isOpen ? "popup popup_opened" : "popup"}>
         <div className={`popup__container popup__container_type_${this.props.name}`}>
           <h2 className="popup__title">{this.props.title}</h2>
           <button
@@ -22,79 +22,7 @@ export default class PopupWithForm extends React.Component {
             name={this.props.name}
             noValidate
           >
-            {this.props.name === "editAvatar" && (
-              <label className="popup__field" htmlFor="avatar-input">
-                <input
-                  id="avatar-input"
-                  className="popup__input popup__input_type_avatar"
-                  type="url"
-                  name="avatar"
-                  placeholder="Ссылка на аватар"
-                  required
-                />
-                <span className="avatar-input-error popup__error"></span>
-              </label>
-            )}
-
-            {this.props.name === "editProfile" && (
-              <>
-                <label className="popup__field" htmlFor="name-input">
-                  <input
-                    id="name-input"
-                    className="popup__input popup__input_type_name"
-                    type="text"
-                    name="name"
-                    placeholder="Имя"
-                    minLength="2"
-                    maxLength="40"
-                    required
-                  />
-                  <span className="name-input-error popup__error"></span>
-                </label>
-                <label className="popup__field" htmlFor="about-input">
-                  <input
-                    id="about-input"
-                    className="popup__input popup__input_type_text"
-                    type="text"
-                    name="about"
-                    placeholder="О себе"
-                    minLength="2"
-                    maxLength="200"
-                    required
-                  />
-                  <span className="about-input-error popup__error"></span>
-                </label>
-              </>
-            )}
-
-            {this.props.name === "addPlace" && (
-              <>
-                <label className="popup__field" htmlFor="place-input">
-                  <input
-                    id="place-input"
-                    className="popup__input popup__input_type_place"
-                    type="text"
-                    name="place"
-                    placeholder="Название"
-                    minLength="2"
-                    maxLength="30"
-                    required
-                  />
-                  <span className="place-input-error popup__error"></span>
-                </label>
-                <label className="popup__field" htmlFor="link-input">
-                  <input
-                    id="link-input"
-                    className="popup__input popup__input_type_link"
-                    type="url"
-                    name="link"
-                    placeholder="Ссылка на картинку"
-                    required
-                  />
-                  <span className="link-input-error popup__error"></span>
-                </label>
-              </>
-            )}
+            {this.props.children}
 
             <button
               className="popup__button popup__button_type_submit"
