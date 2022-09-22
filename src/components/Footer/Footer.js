7d8623ddbb265f53d8035b21102a53dea0653364
Marsel Ishmukhametov <yamarvin01@yandex.ru>
2022-09-22 09:54:03 +0300

    PW10 - Исправление замечаний ревью

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,17 +1,3 @@
-// // Функциональный компонент Footer
-// function Footer() {
-//   return (
-//     <footer className="footer">
-//       <p className="footer__copyright">&copy; 2020 Mesto Russia</p>
-//     </footer>
-//   );
-// }
-
-// export default Footer;
-
-
-
-// Классовый компонент Footer
 import React from "react";
 
 export default class Footer extends React.Component {
