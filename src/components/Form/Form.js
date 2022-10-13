fd2b789927059f65ed78dc7f359384a8e5c59cbc
Marsel Ishmukhametov <yamarvin01@yandex.ru>
2022-10-14 00:38:47 +0300

    PW11 - Добавить удаление карточек

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -5,9 +5,14 @@ export default class Form extends React.Component {
     super(props);
   }
 
+  handleSubmit(evt) {
+    evt.preventDefault();
+  }
+
   render() {
     return (
       <form
+        onSubmit={this.handleSubmit}
         className={`popup__form popup__form_type_${this.props.name}`}
         name={this.props.name}
         noValidate
