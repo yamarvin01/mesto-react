48f011c9543b6b2ecb31ed5b12426e62fb8164f6
Marsel Ishmukhametov <yamarvin01@yandex.ru>
2022-10-14 17:22:07 +0300

    PW11 - Добавить удаление карточек

--- src/components/EditProfilePopup/EditProfilePopup.js
+++ src/components/EditProfilePopup/EditProfilePopup.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PopupWithForm from "../PopupWithForm/PopupWithForm.js";
+import Input from "../Input/Input.js";
 import { CurrentUserContext } from "../context/CurrentUserContext.js";
 
 export default function EditProfilePopup(props) {
@@ -28,36 +29,24 @@ export default function EditProfilePopup(props) {
       isOpen={props.isOpen}
       onClose={props.onClose}
     >
-      <label className="popup__field" htmlFor="name">
-        <input
-          onChange={handleNameChange}
-          value={nameValue}
-          className="popup__input popup__input_type_name"
-          name="name"
-          id="name"
-          type="text"
-          placeholder="Имя"
-          minLength="2"
-          maxLength="40"
-          required
-        />
-        <span className="about-input-error popup__error"></span>
-      </label>
-      <label className="popup__field" htmlFor="about">
-        <input
-          onChange={handleAboutChange}
-          value={aboutValue}
-          className="popup__input popup__input_type_about"
-          name="about"
-          id="about"
-          type="text"
-          placeholder="О себе"
-          minLength="2"
-          maxLength="200"
-          required
-        />
-        <span className="about-input-error popup__error"></span>
-      </label>
+      <Input
+        value={nameValue}
+        onChange={handleNameChange}
+        name={"name"}
+        type={"text"}
+        placeholder={"Имя"}
+        minLength={"2"}
+        maxLength={"40"}
+      />
+      <Input
+        value={aboutValue}
+        onChange={handleAboutChange}
+        name={"about"}
+        type={"text"}
+        placeholder={"О себе"}
+        minLength={"2"}
+        maxLength={"200"}
+      />
     </PopupWithForm>
   );
 }
