ef3994a0bad09e802187bbf3dbeb8b16468d8617
Marsel Ishmukhametov <yamarvin01@yandex.ru>
2022-10-15 19:18:23 +0300

    PW11 - Добавить закрытие popup черзе Click и Esc

--- src/components/ImagePopup/ImagePopup.js
+++ src/components/ImagePopup/ImagePopup.js
@@ -1,30 +1,23 @@
-import React from "react";
 import ButtonClose from "../ButtonClose/ButtonClose";
 
-export default class ImagePopup extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div
-        className={
-          this.props.isOpen
-            ? `popup popup_dark popup_type_${this.props.name} popup_opened`
-            : `popup popup_dark popup_type_${this.props.name}`
-        }
-      >
-        <div className="popup__container popup__container_type_image">
-          <ButtonClose onClose={this.props.onClose} />
-          <img
-            className="popup__image"
-            alt={`Изображение ${this.props.card.name}`}
-            src={this.props.card.link}
-          />
-          <p className="popup__text">{this.props.card.name}</p>
-        </div>
+export default function ImagePopup(props) {
+  return (
+    <div
+      className={
+        props.isOpen
+          ? `popup popup_dark popup_type_${props.name} popup_opened`
+          : `popup popup_dark popup_type_${props.name}`
+      }
+    >
+      <div className="popup__container popup__container_type_image">
+        <ButtonClose onClose={props.onClose} />
+        <img
+          className="popup__image"
+          alt={`Изображение ${props.card.name}`}
+          src={props.card.link}
+        />
+        <p className="popup__text">{props.card.name}</p>
       </div>
-    );
-  }
+    </div>
+  );
 }
