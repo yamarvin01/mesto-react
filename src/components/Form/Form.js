a8b52cc9e0f4aa72af5db8265c986736981f0389
Marsel Ishmukhametov <yamarvin01@yandex.ru>
2022-10-16 18:15:55 +0300

    PW11 - Исправление замечаний Ревью

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -4,8 +4,6 @@ export default function Form(props) {
       onSubmit={props.onSubmit}
       className={`popup__form popup__form_type_${props.name}`}
       name={props.name}
-      // TODO: настроить валидацию
-      // noValidate
     >
       {props.children}
     </form>
